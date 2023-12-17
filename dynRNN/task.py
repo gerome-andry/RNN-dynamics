e23f35a05a6c7b7924683dfaea17adbd2041b447
aurelienbernard <aurelien.bernard@student.uliege.be>
2023-12-17 16:09:25 +0100

    added TimedSpatialReproductionTask

--- dynRNN/task.py
+++ dynRNN/task.py
@@ -339,19 +339,110 @@ class IntervalComparisonTask(Task):
         
 class TimedSpatialReproductionTask(Task):
     
-    def get_batch(n, min_t1_length=10, max_t1_length=20,
-                     min_T_length=10, max_T_length=100,
-                     min_t2_length=10, max_t2_length=100,
-                     min_t3_length= 10, max_t3_length=20,
-                     t_padding=10):
-
-        return None
-
-    def loss(target, pred):
-        return 0
+    # In this task:
+    # the network has 34 inputs. 32 are neurons that code for location and 2 for scalar pulses.
+    # 1 - Network perceives a pulse with location cetered around a neuron of duration "pulses_duration",
+    # 2 - After an interval T after the pulse with location a second scalar pulse of amplitude 1 and duration "pulses_duration", is sent
+    # 3 - Then there is a delay interval of variable duration t2,
+    # 4 - network receives a "go" scalar pulse of same aplitude and duration,
+    # 4 - the network, after a time T, should produce a step centered at the same neuron as the first pulse.
     
+    def get_batch(n, dt = 20, # dt in ms
+                  min_t1_length=60, max_t1_length=500, # in ms
+                  min_T_length=400, max_T_length=1400,# in ms
+                  min_t2_length=600, max_t2_length=1600, # in ms
+                  pulses_duration=60, padding_duration = 300, # in ms
+                  add_noise = False, noise_stdev = 0.01):
+        
+        pulses_duration_ts = pulses_duration // dt
+        max_sequence_length = max_t1_length + pulses_duration + max_T_length + pulses_duration + max_t2_length + max_T_length + padding_duration
+        max_sequence_length //= dt
+        
+        pulses_duration_ts = pulses_duration // dt
+        
+        max_sequence_length = max_t1_length + pulses_duration + max_T_length + pulses_duration + max_t2_length + max_T_length + padding_duration
+        max_sequence_length //= dt
+        
+        t1_len = torch.randint(min_t1_length, max_t1_length, (n, 1)) // dt
+        T_len = torch.randint(min_T_length, max_T_length, (n, 1))    // dt 
+        t2_len = torch.randint(min_t2_length, max_t2_length, (n, 1)) // dt
+        
+        pulse_1_begin_indices = t1_len + torch.zeros((n, max_sequence_length), dtype=torch.long)
+        pulse_1_end_indices = pulse_1_begin_indices + pulses_duration_ts
+        
+        pulse_2_begin_indices = pulse_1_end_indices + T_len
+        pulse_2_end_indices =  pulse_2_begin_indices + pulses_duration_ts
+        
+        pulse_go_begin_indices = pulse_2_end_indices + t2_len
+        pulse_go_end_indices = pulse_go_begin_indices + pulses_duration_ts
+
+        inputs_sequences = torch.zeros((n,max_sequence_length,34)) #shape (batch_size,sequence_length,34)
+
+        # Create a mask for setting the continuous range for pulse 1
+        mask_pulse_1 = (torch.arange(max_sequence_length).unsqueeze(0) >= pulse_1_begin_indices) & (torch.arange(max_sequence_length).unsqueeze(0) < pulse_1_end_indices)
+        mask_pulse_1 = mask_pulse_1.unsqueeze(-1).to(torch.float)
+        
+        location_stimulus = TimedSpatialReproductionTask.generate_multiple_gaussians(n, 32, 6, 26, 2)
+        
+        # Create a mask for setting the continuous range for pulse 2
+        mask_pulse_2 = (torch.arange(max_sequence_length).unsqueeze(0) >= pulse_2_begin_indices) & (torch.arange(max_sequence_length).unsqueeze(0) < pulse_2_end_indices)
+        mask_pulse_2 = mask_pulse_2.unsqueeze(-1).to(torch.float)
+        
+        # Create a mask for setting the continuous range for pulse go
+        mask_pulse_go = (torch.arange(max_sequence_length).unsqueeze(0) >= pulse_go_begin_indices) & (torch.arange(max_sequence_length).unsqueeze(0) < pulse_go_end_indices)
+        mask_pulse_go = mask_pulse_go.unsqueeze(-1).to(torch.float)
+
+        inputs_sequences[:, :, :32] += mask_pulse_1 * location_stimulus.unsqueeze(1)
+        inputs_sequences[:,:,32:] += mask_pulse_2 + mask_pulse_go
+        
+        if add_noise:
+            inputs_sequences = torch.normal(inputs_sequences, noise_stdev)
+        
+        target_outputs = torch.zeros((n,max_sequence_length,32))
+        target_indices = pulse_go_end_indices + T_len
+        
+        target_mask = (torch.arange(max_sequence_length).unsqueeze(0) >= target_indices)
+        target_mask = target_mask.unsqueeze(-1).to(torch.float)
+
+        # Set the continuous range between t2_indices and target_indices to 1 in the target sequence
+        target_outputs += target_mask * location_stimulus.unsqueeze(1)
+
+        return inputs_sequences, target_outputs
+    
+    def generate_multiple_gaussians(L, n, range_min, range_max, sigma):
+        indices = torch.arange(0, n, 1, dtype=torch.float32)
+        centers = torch.randint(range_min, range_max, (L,))
+        
+        # Expand dimensions for broadcasting
+        indices = indices.unsqueeze(0)  # Shape: (1, n)
+        centers = centers.unsqueeze(1)  # Shape: (L, 1)
+        
+        # Calculate Gaussian curves using broadcasting
+        gaussians = torch.exp(-0.5 * ((indices - centers) / sigma) ** 2)
+        
+        return gaussians
+    
+    def loss(target, pred):
+        return ((target - pred)**2).mean()
+
     def plot_sequences(batch_input, batch_target):
-        return None
+        batch_size, sequence_length, sequence_dimensions = batch_input.size()
+
+        for i in range(batch_size):
+            fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 12))
+
+            im1 = ax1.imshow(batch_input[i].T, cmap='viridis', interpolation='nearest')
+            ax1.set_title(f'Batch input {i+1} Heatmap')
+            ax1.set_xlabel('Sequence Length')
+            ax1.set_ylabel('Sequence Dimensions')
+
+            im2 = ax2.imshow(batch_target[i].T, cmap='viridis', interpolation='nearest')
+            ax2.set_title(f'Batch target {i+1} Heatmap')
+            ax2.set_xlabel('Sequence Length')
+            ax2.set_ylabel('Sequence Dimensions')
+
+            plt.tight_layout()
+            plt.show()
 
     
 if __name__ == '__main__':
@@ -361,5 +452,12 @@ if __name__ == '__main__':
     #IntervalProductionTask.plot_sequences(batch_input,batch_target)
     
     ### test of Interval comparison task
-    batch_input, batch_target = IntervalComparisonTask.get_batch(5)
-    IntervalComparisonTask.plot_sequences(batch_input,batch_target)
+    #batch_input, batch_target = IntervalComparisonTask.get_batch(5)
+    #IntervalComparisonTask.plot_sequences(batch_input,batch_target)
+    
+    ### test of TimedSpatialReproductionTask
+    #batch_input, batch_target = TimedSpatialReproductionTask.get_batch(1)
+    #TimedSpatialReproductionTask.plot_sequences(batch_input,batch_target)
+    
+    ###
+    
